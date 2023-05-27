e3705190f889bd4532a3c4c030b0bde946f8cf56
Mattia Marelli <sesquialtera87@gmail.com>
2023-05-27 16:41:43 +0200

    Dark theme

---
+++ my_extensions/win_dark_theme/__init__.py
@@ -0,0 +1,30 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPalette, QColor
+
+# noinspection PyUnresolvedReferences
+import extensions, app
+
+
+def dark_theme():
+    palette = QPalette()
+    palette.setColor(QPalette.Window, QColor(53, 53, 53))
+    palette.setColor(QPalette.WindowText, Qt.white)
+    palette.setColor(QPalette.Base, QColor(25, 25, 25))
+    palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+    palette.setColor(QPalette.ToolTipBase, Qt.white)
+    palette.setColor(QPalette.ToolTipText, Qt.white)
+    palette.setColor(QPalette.Text, Qt.white)
+    palette.setColor(QPalette.Button, QColor(53, 53, 53))
+    palette.setColor(QPalette.ButtonText, Qt.white)
+    palette.setColor(QPalette.BrightText, Qt.red)
+    palette.setColor(QPalette.Link, QColor(42, 130, 218))
+    palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+    palette.setColor(QPalette.HighlightedText, Qt.black)
+    app.qApp.setPalette(palette)
+
+
+class Extension(extensions.Extension):
+
+    def __init__(self, parent, name):
+        super(Extension, self).__init__(parent, name)
+        dark_theme()
